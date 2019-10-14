80278410c2bb1358ff5fc07a938a66706f3cb76c
TomyJPN <masaya120331@gmail.com>
2019-10-14 19:12:42 +0900

    各シーンの遷移記述

---
+++ LiliaAndTheRainbowNocturne/Assets/Project/Home/NavBar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavBar : SingletonMonoBehaviour<NavBar> {
+  public void Awake() {
+    if (this != Instance) {
+      Destroy(this);
+      return;
+    }
+    DontDestroyOnLoad(this.gameObject);
+  }
+  
+  void Start() {
+
+  }
+
+  public void onHomeBtn() {
+    if (SceneManager.GetActiveScene().name == "Home") {
+      Debug.Log(SceneManager.GetActiveScene().name);
+      return;
+    }
+    SceneManager.LoadScene("Home");
+  }
+  public void onCharaBtn() {
+    if (SceneManager.GetActiveScene().name == "Chara") {
+      Debug.Log(SceneManager.GetActiveScene().name);
+      return;
+    }
+    SceneManager.LoadScene("Chara");
+  }
+  public void onStoryBtn() {
+    if (SceneManager.GetActiveScene().name == "StoryMenu") {
+      Debug.Log(SceneManager.GetActiveScene().name);
+      return;
+    }
+    SceneManager.LoadScene("StoryMenu");
+  }
+  public void onQuestBtn() {
+    if (SceneManager.GetActiveScene().name == "Quest") {
+      Debug.Log(SceneManager.GetActiveScene().name);
+      return;
+    }
+    SceneManager.LoadScene("Quest");
+  }
+  public void onGatyaBtn() {
+    if (SceneManager.GetActiveScene().name == "Gatya") {
+      Debug.Log(SceneManager.GetActiveScene().name);
+      return;
+    }
+    SceneManager.LoadScene("Gatya");
+  }
+  public void onMenuBtn() {
+    if (SceneManager.GetActiveScene().name == "Menu") {
+      Debug.Log(SceneManager.GetActiveScene().name);
+      return;
+    }
+    SceneManager.LoadScene("Menu");
+  }
+}
